0c22ad984f15d56c6697084b57b65fb241c5143c
Ярослав Попов <popow.yaroslaw2012@yandex.ru>
2026-06-30 19:46:53 +0500

    Проделанная работа

--- config/settings.py
+++ config/settings.py
@@ -145,3 +145,23 @@ SPECTACULAR_SETTINGS = {
 }
 
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
+
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
+
+
+TIME_ZONE = "Europe/Moscow"
+USE_TZ = True
+CELERY_TIMEZONE = "Europe/Moscow"
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
+
+CELERY_BEAT_SCHEDULE = {
+    'check_inactive_users_every_day': {
+        'task': 'users.tasks.check_inactive_users',
+        'schedule': 24 * 60 * 60.0,
+    },
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST_USER = 'noreply@mycourseplatform.ru'
